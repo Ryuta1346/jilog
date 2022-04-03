bc7eae0c63027204823ae032fe23757951290996
Ryuta <ryuta.tashiro@gmail.com>
2022-04-04 00:36:34 +0900

    add constructor (#4)
    * add typescript & update eslint
    
    * add jilog
    
    * fix setting
    
    * add build setting
    
    * use rollup
    
    * fix prettier
    
    * fix lint
    
    * fix
    
    * add script
    
    * remove unnecessary code
    
    * fix constructor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { jiLog, jiInfo, jiWarn, jiError } from "./jilog";
+export { jiLog, jiInfo, jiWarn, jiError } from './jilog'
--- src/jilog.ts
+++ src/jilog.ts
@@ -1,12 +1,23 @@
 class JiLog {
-  private logCount = 0
-  private infoLogCount = 0
-  private warnLogCount = 0
-  private errorLogCount = 0
-  private logTime: number = window.performance.now()
-  private infoTime: number = window.performance.now()
-  private warnTime: number = window.performance.now()
-  private errorTime: number = window.performance.now()
+  private logCount: number
+  private infoLogCount: number
+  private warnLogCount: number
+  private errorLogCount: number
+  private logTime: number
+  private infoTime: number
+  private warnTime: number
+  private errorTime: number
+
+  constructor() {
+    this.logCount = 0
+    this.infoLogCount = 0
+    this.warnLogCount = 0
+    this.errorLogCount = 0
+    this.logTime = performance.now()
+    this.infoTime = performance.now()
+    this.warnTime = performance.now()
+    this.errorTime = performance.now()
+  }
 
   log(value: any, label = '', color = '') {
     console.log(
